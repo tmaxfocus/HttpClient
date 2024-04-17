208a9bff28a32a128a4647143021c4d549251a20
Tobi Adeogun <Tobi.Adeogun@stanbicibtc.com>
2024-04-17 17:17:34 +0100

    Json serilization

---
+++ Movies.Client/Helpers/JsonSerializerOptionsWrapper.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.Json;
+using System.Threading.Tasks;
+
+namespace Movies.Client.Helpers
+{
+    public class JsonSerializerOptionsWrapper
+    {
+        public JsonSerializerOptions Options { get; }
+
+        public JsonSerializerOptionsWrapper()
+        {
+            Options = new JsonSerializerOptions(JsonSerializerDefaults.Web);
+        }
+    }
+}
--- Movies.Client/Services/CRUDSamples.cs
+++ Movies.Client/Services/CRUDSamples.cs
@@ -1,5 +1,6 @@
 ﻿
 
+using Movies.Client.Helpers;
 using Movies.Client.Models;
 using System.Text.Json;
 using System.Xml.Serialization;
@@ -9,22 +10,50 @@ namespace Movies.Client.Services;
 public class CRUDSamples : IIntegrationService
 {
     private readonly IHttpClientFactory _httpClientFactory;
+    private readonly JsonSerializerOptionsWrapper _jsonSerializerOptionsWrapper;
 
-    public CRUDSamples(IHttpClientFactory httpClientFactory)
+    public CRUDSamples(IHttpClientFactory httpClientFactory,
+        JsonSerializerOptionsWrapper jsonSerializerOptionsWrapper)
     {
+        _jsonSerializerOptionsWrapper = jsonSerializerOptionsWrapper?? throw new ArgumentNullException(nameof(jsonSerializerOptionsWrapper));
         _httpClientFactory = httpClientFactory?? throw new ArgumentNullException(nameof(httpClientFactory));
     }
 
     public async Task RunAsync()
     {
         await GetResourceAsync<Movie>();
+        await GetResourceThroughHttpRequestMessageAsync<Movie>();
+    }
+
+
+    public async Task GetResourceThroughHttpRequestMessageAsync<T>()
+    {
+        var httpClient = _httpClientFactory.CreateClient("MoviesAPIClient");
+        //httpClient.BaseAddress = new Uri("http://localhost:5001");
+        //httpClient.Timeout = new TimeSpan(0, 0, 30);
+
+        // enable request message handler
+
+        var request = new HttpRequestMessage(HttpMethod.Get, "api/movies");
+
+        // enble the request header
+        request.Headers.Accept.Add(new System.Net.Http.Headers.MediaTypeWithQualityHeaderValue("application/json"));
+
+        var response = await httpClient.SendAsync(request);
+
+        response.EnsureSuccessStatusCode();
+
+        var content = await response.Content.ReadAsStringAsync();
+
+        IEnumerable<T> movies = new List<T>();
+        movies = JsonSerializer.Deserialize<IEnumerable<T>>(content, _jsonSerializerOptionsWrapper.Options);
     }
 
     public async Task GetResourceAsync<T>() 
     {
-        var httpClient = _httpClientFactory.CreateClient();
-        httpClient.BaseAddress = new Uri("http://localhost:5001");
-        httpClient.Timeout = new TimeSpan(0, 0, 30);
+        var httpClient = _httpClientFactory.CreateClient("MoviesAPIClient");
+       // httpClient.BaseAddress = new Uri("http://localhost:5001");
+        //httpClient.Timeout = new TimeSpan(0, 0, 30);
 
         httpClient.DefaultRequestHeaders.Clear();
         httpClient.DefaultRequestHeaders.Accept.Add(new System.Net.Http.Headers.MediaTypeWithQualityHeaderValue("application/xml"));
@@ -36,10 +65,7 @@ public class CRUDSamples : IIntegrationService
         IEnumerable<T> movies = new List<T>();
         if(respose.Content.Headers.ContentType?.MediaType == "application/json")
         {
-            movies = JsonSerializer.Deserialize<IEnumerable<T>>(content, new JsonSerializerOptions
-            {
-                PropertyNamingPolicy = JsonNamingPolicy.CamelCase
-            });
+            movies = JsonSerializer.Deserialize<IEnumerable<T>>(content, _jsonSerializerOptionsWrapper.Options);
         }
 
         if (respose.Content.Headers.ContentType?.MediaType == "application/xml")
